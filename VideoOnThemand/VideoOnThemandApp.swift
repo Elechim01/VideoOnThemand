c680a8f8a8d40a491faac592a547cad8af7b75cd
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2022-08-09 16:02:54 +0200

    Initial Commit

---
+++ VideoOnThemand/VideoOnThemandApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoOnThemandApp.swift
+//  VideoOnThemand
+//
+//  Created by Michele Manniello on 09/08/22.
+//
+
+import SwiftUI
+
+@main
+struct VideoOnThemandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
